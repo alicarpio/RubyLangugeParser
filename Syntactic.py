c1cc8e6c5af1f43dc66303d99befe9a72d0411e6
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-11-16 19:07:57 -0500

    Corrections in syntactic.py

--- Syntactic.py
+++ Syntactic.py
@@ -3,14 +3,12 @@ from Lexer import tokens
 
 def p_asignacion(p):
     'asignacion : VARIABLE_LOCAL EQUALS valor'
-    p[0] = (p[1], p[2], p[3])  # Ejemplo de acción semántica para esta regla
 
 def p_valor(p):
     '''valor : STRING
              | INTEGER
              | FLOAT
              | VARIABLE_LOCAL'''
-    p[0] = p[1]  # Asignar el valor reconocido al elemento de la pila semántica
 
 # Error rule for syntax errors
 def p_error(p):
